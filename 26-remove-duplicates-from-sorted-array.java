4b211d45dcbdbc290916c1808b48b72636c2b74f
Yiran Mao <YMao@SymbolicIO.com>
2015-08-05 11:43:33 -0400

    Date: 8/05/2015 26. Remove Duplicates from Sorted Array (2/5)

--- 26-remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array.java
@@ -1,19 +1,13 @@
 public class Solution {
     public int removeDuplicates(int[] nums) {
-        if(nums.length == 0) {
-            return 0;
-        }
+        if(nums.length == 0) return 0;
         
-        int pivot = 0,
-            runner = 1;
-        while(runner < nums.length) {
+        int pivot = 0, runner = 0;
+        while(++runner < nums.length) {
             if(nums[runner] != nums[pivot]) {
-                pivot++;
-                nums[pivot] = nums[runner];
+                nums[++pivot] = nums[runner];
             }
-            runner++;
         }
-
         return pivot + 1;
     }
 }

040f87ca8597feee3f47a4b89efe792678a7d8a3
Yiran Mao <yiranmao@gmail.com>
2015-08-13 15:05:04 -0400

    Date: 8/13/2015 121. best time to buy and sell stock修改了算法，通过了题目对复杂度的要求

--- 121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock.java
@@ -1,12 +1,12 @@
 public class Solution {
   public int maxProfit(int[] prices) {
+    if(prices.length == 0) return 0;
+    
   	int maxProfit = 0;
-  	for(int i = 0; i < prices.length; i++) {
-  		for(int j = i; j < prices.length; j++) {
-  			if(prices[j] - prices[i] > maxProfit) {
-  				maxProfit = prices[j] - prices[i];
-  			}
-  		}
+  	int minPrice = prices[0];
+  	for(int i = 1; i < prices.length; i++) {
+  	  minPrice = Math.min(prices[i], minPrice);
+  	  maxProfit = Math.max(prices[i] - minPrice, maxProfit);
   	}
   	return maxProfit;    
   }
